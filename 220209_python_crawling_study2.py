0f53de096ea9bcbc4f367ebe6b34c70639714a46
Byun Yoon.Sub <crid2450@gmail.com>
2022-02-09 22:34:47 +0900

    파일 입출력 추가

--- 220209_python_crawling_study2.py
+++ 220209_python_crawling_study2.py
@@ -6,14 +6,14 @@ target = req.urlopen(new)
 soup = BeautifulSoup(target, "html.parser")
 
 news = soup.select("strong.tit_g") # . 이 class를 의미한다. strong class ="tit_g" = "strong.tit_g"
-for list in news :
-   a = list.select_one("a")
-   print("링크 : " + a.attrs['href'])
-   # file.write("링크 : " + a.attrs['href'] + "\n")
-   title = a.string.strip() # .strip 공백 제거
-   print("제목 : " , title)
-   # file.write("제목 : " + title  + "\n")
-
 with open("news.txt" , "w", encoding="utf8") as f:
-# file.write("링크 : " + a.attrs['href'] + "\n")
-# file.write("제목 : " + title  + "\n")
+   for list in news :
+      a = list.select_one("a")
+      print("링크 : " + a.attrs['href'])
+      f.write("링크 : " + a.attrs['href'] + "\n")
+      title = a.string.strip() # .strip 공백 제거
+      print("제목 : " , title)
+      f.write("제목 : " + title + "\n")
+
+
+
