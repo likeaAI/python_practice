e6f3394c2d1505cb2cf1bfec21116d63b8ee86b2
Byun Yoon Sub <98403290+likeaAI@users.noreply.github.com>
2022-02-11 22:25:38 +0900

    Delete 210211 python_practice.py

--- 210211 python_practice.py
+++
@@ -1,9 +0,0 @@
-
-# 마지막 줄을 출력하는 코드
-def get_final_line():
-    final_line =''
-    for current_line in final_line :
-        final_line = current_line
-        return final_line
-
-get_final_line('.\data\palindrome_words.txt')
