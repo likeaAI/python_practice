ccebabb2cbdf3f6c4eb56d84a37c74114b016189
Byun Yoon.Sub <crid2450@gmail.com>
2022-03-05 19:02:53 +0900

    --- 동영상도 플레이리스트로 다운받는 코드 구현 남음

--- self.homework/3three game/youtubedownloader.py
+++ self.homework/3three game/youtubedownloader.py
@@ -1,6 +1,8 @@
 import pytube
 from moviepy.editor import *
 from pytube import Playlist
+import time
+
 
 print("다운로드 유형을 고르세요 ")
 print("1번 영상 하나 url 타입  ")
@@ -24,18 +26,18 @@ if main == 1 :
 
     #비디오 다운로드(해상도 가장 좋은 것으로 필터)
     yt.streams.filter(adaptive=True, file_extension='mp4', only_video=True).order_by\
-        ('resolution').desc().first().download('D:\DOWNLOAD2\youtube\only video', yt.title)
+        ('resolution').desc().first().download('D:\DOWNLOAD2\youtube\only_video', yt.title[0:4])
     #오디오 다운로드
     yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).order_by('abr').desc().first()\
-        .download('D:\DOWNLOAD2\youtube\only sound', yt.title)
+        .download('D:\DOWNLOAD2\youtube\only_sound', yt.title[0:4])
      # 다운받는 파일 경로
-    videoclip = VideoFileClip("D:\\DOWNLOAD2\\youtube\\only video\\" + yt.title)
-    audioclip = AudioFileClip("D:\\DOWNLOAD2\\youtube\\only sound\\" + yt.title)
+    videoclip = VideoFileClip("D:\\DOWNLOAD2\\youtube\\only_video\\" + yt.title[0:4])
+    audioclip = AudioFileClip("D:\\DOWNLOAD2\\youtube\\only_sound\\" + yt.title[0:4])
     # 오디오를 먼저 불러온다.
     videoclip.audio = audioclip
     # 그 위에 영상을 합쳐서 파일이름으로 저장한다.
     # (경로를 지정해줘도 된다. 지정하지 않으면 자동으로 파이썬 파일이 있는 곳에 저장됨)
-    videoclip.write_videofile("D:\\DOWNLOAD2\\youtube\\" + yt.title +".mp4")
+    videoclip.write_videofile("D:\\DOWNLOAD2\\youtube\\" + yt.title[0:4] +".mp4")
 
 
 else :
@@ -44,6 +46,6 @@ else :
     p2 = Playlist(url2)
     for video in p2.videos:
 
-        video.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).order_by('abr').desc().first() \
+        video.streams.filter(adaptive=True, file_extension='mp3', only_audio=True).order_by('abr').desc().first() \
             .download('D:\DOWNLOAD2\youtube\downloadmusic')
 
