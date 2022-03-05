0a6377f2875dd01f538a98d7855a68e87dd07518
Byun Yoon.Sub <crid2450@gmail.com>
2022-03-05 13:52:25 +0900

    유튜브 다운로더

--- self.homework/3three game/youtubedownloader.py
+++ self.homework/3three game/youtubedownloader.py
@@ -1,36 +1,36 @@
-from pytube import YouTube
-from pytube import Playlist
-DOWNLOAD_FOLDER = "D:\DOWNLOAD2\youtube"
+import pytube
+from moviepy.editor import *
 
-print("다운로드 유형을 고르세요 ")
-print("1번 영상 하나 url 타입  ")
-print("2번 영상 하나 플레이리스트 타입  ")
+url = input("다운받을 유튜브주소를 입력 : ")
+yt = pytube.YouTube(url)
 
-main = int(input())
-if main == 1 :
-        url = input("다운받을 주소를 복사해서 넣어보자. :  ")
-        yt = YouTube(url)
+file_name = yt.title
+print(file_name)
 
-        stream = yt.streams.get_highest_resolution()
-        stream.download(DOWNLOAD_FOLDER)
+#유튜브 객체에서 스트리밍 관련 모든 것 가져오기
 
 
-        print("제목 : ", yt.title)
-        print("길이 : ", yt.length)
-        print("게시자 : ", yt.author)
-        print("게시날짜 : ", yt.publish_date)
-        print("조회수 : ", yt.views)
-        print("키워드 : ", yt.keywords)
-        print("설명 : ", yt.description)
-        print("썸네일 : ", yt.thumbnail_url)
 
+#리스트 프린트해주기
 
-else :
-    inputplay = input("플레이스 리스트 주소를 복사해서 넣어라 ")
-    p = Playlist(inputplay)
-    for video in p.videos:
-        video.streams.first().download(DOWNLOAD_FOLDER)
+#비디오 다운로드(해상도 가장 좋은 것으로 필터)
+yt.streams.filter(adaptive=True, file_extension='mp4', only_video=True).order_by\
+    ('resolution').desc().first().download('D:\DOWNLOAD2\youtube\only video', yt.title)
+
+#오디오 다운로드
+yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).order_by('abr').desc().first()\
+    .download('D:\DOWNLOAD2\youtube\only sound', yt.title)
 
 
 
 
+ # 다운받는 파일 경로
+videoclip = VideoFileClip("D:\DOWNLOAD2\youtube\only video" , yt.title)
+audioclip = AudioFileClip("D:\DOWNLOAD2\youtube\only sound" , yt.title)
+
+# 오디오를 먼저 불러온다.
+videoclip.audio = audioclip
+
+# 그 위에 영상을 합쳐서 파일이름으로 저장한다.
+# (경로를 지정해줘도 된다. 지정하지 않으면 자동으로 파이썬 파일이 있는 곳에 저장됨)
+videoclip.write_videofile(yt.tilte + ".mp4")
