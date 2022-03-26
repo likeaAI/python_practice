8d1b9e39028dc61355f48373a103fbcda32f2b22
YoonSub <lefantopera@gmail.com>
2022-03-27 00:27:22 +0900

    -- 210326

--- self.homework/220326 python_practice04.py
+++ self.homework/220326 python_practice04.py
@@ -11,6 +11,26 @@ print(test02)
 test03 = {x*x for x in range(5)}
 print(test03)
 
-# str.join !!!
+
+test04 = [x*x
+          for x in range(5)
+          if x%2 ]
+print(test04)
+
+
+test05 = [(x,y) for x in range(5) if x%2 \
+          for y in range(5) if y%3]
+
+print(test05)
+
+words = 'this is a bunch of words.'.split()
+x = map(len, words)
+print(sum(x))
+
+def is_a_long_word(one_word) :
+    return len(one_word) > 4
+x = filter(is_a_long_word, words)
+print(''.join(x))
+
 
 
