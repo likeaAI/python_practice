fbf98457e86a41f1faff04fb61c84200391733a8
YoonSub <lefantopera@gmail.com>
2022-03-31 23:47:36 +0900

    -- 210326

--- self.homework/0331 python_practioce.py
+++ self.homework/0331 python_practioce.py
@@ -39,3 +39,26 @@ def wordcount() :
         print(f'{key} : {value}')
 
 wordcount()
+
+
+# 파일에서 가장 긴 단어 찾기
+
+import os
+def find_longest_word(filename) :
+    longest_word =''
+    for one_line in open(filename):
+        for one_word in one_line.split():
+            if len(one_word) > len(longest_word) :
+                longest_word = one_word
+    return longest_word
+
+def find_all_longgest_words(dirname) :
+    return {
+        filename :
+            find_longest_word(os.path.join(dirname, filename))
+            for filename in os.listdir(dirname)
+            if os.path.isfile(os.path.join(dirname,filename))
+    }
+print(find_all_longgest_words(''))
+
+
