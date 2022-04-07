343d831a8a8629df2eebda618279c44f1f6816a0
YoonSub <lefantopera@gmail.com>
2022-04-07 20:50:06 +0900

    - 웹크롤링 pd.read_html 이용 방법 후 pd.DataFrame 작성법

---
+++ self.homework/0407 webcrolling.py
@@ -0,0 +1,11 @@
+
+
+import pandas as pd
+import numpy as np
+
+
+tables = pd.read_html('https://www.koreabaseball.com/Record/Player/HitterBasic/Basic1.aspx')
+print(tables)
+
+df = pd.DataFrame(tables[0] , columns=['순위' , '선수명' , '팀명' , 'AVG'])
+print(df)
