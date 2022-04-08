2612d753050e17eea07938889b2f9c7d32dbde91
YoonSub <lefantopera@gmail.com>
2022-04-08 23:40:17 +0900

    - 웹크롤링 pd.read_html 이용 방법 후 pd.DataFrame 작성법

--- self.homework/220408 practice.py
+++ self.homework/220408 practice.py
@@ -9,14 +9,28 @@ url = 'https://comic.naver.com/index'
 webtoon = requests.get(url)
 soup = BeautifulSoup(webtoon.text , 'lxml')
 
+
 lists = soup.find_all('div' > 'li', attrs={'class' : 'asideBoxRank'} )
-lists_01 = soup.select(' .asideBoxRank')
+
+url2 = 'https://movie.daum.net/ranking/boxoffice/weekly'
+daum_movie = requests.get(url2)
+soup2 = BeautifulSoup(daum_movie.text , 'lxml')
 
 
-print(lists_01)
-webtoon_list = []
-for list in range(len(lists_01)) :
-    webtoon_list.append(lists_01[list].get_text())
+daum_movie_list = []
+daum_movie_open = []
+movie_lists = soup2.select(' .tit_item a')
+for movie in movie_lists :
+   daum_movie_list.append(movie.get_text())
 
-print(webtoon_list)
+open_date = soup2.select( '.txt_num')
+for date in open_date :
+   daum_movie_open.append(date.get_text())
 
+
+
+daum_report = pd.DataFrame({'open' : daum_movie_open , 'title' : daum_movie_list})
+daum_report['ranking'] = [i for i in range(1,16)]
+daum_report.index = daum_report['ranking']
+daum_report.drop('ranking' , axis=1)
+print(daum_report)
