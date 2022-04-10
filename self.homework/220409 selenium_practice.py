aa4ee57aac6b963056f2b6f27dd0331b52b4b75e
YoonSub <lefantopera@gmail.com>
2022-04-10 19:18:21 +0900

    - 웹크롤링 pd.read_html 이용 방법 후 pd.DataFrame 작성법

---
+++ self.homework/220409 selenium_practice.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import numpy  as np
+import pandas as pd
+import requests
+import re
+from time    import sleep , time
+from random  import randint
+
+
+
+# 일동제약 홈페이지에서 셀레늄으로 상품 웹크롤링을 해보자.
+# 크롬 세팅
+path = 'C:/Users/crid2/driver/chromedriver.exe'
+driver = webdriver.Chrome(path)
+driver.get('https://www.ildong.com/kor/product/list.id?page=1&halt=&prdDisease=&prdCategory=&searchVal=&searchOption=0')
+
+for page in range(1, 3):
+    print('page - ', page)
+    driver.get('https://www.ildong.com/kor/product/list.id?page=' + str(
+        page) + '&halt=&prdDisease=&prdCategory=&searchVal=&searchOption=0')
+    sleep(3)
+
+    list = driver.find_element_by_class_name('prList')  # <div class='prlist' >
+    a_tags = list.find_elements_by_tag_name('a')
+
+    # print( len(a_tags) )
+    for idx in range(len(a_tags)):
+        a_tags[idx].click()
+        detail = driver.find_element_by_class_name('detailCnt2')
+        print(detail.text)
+        print('*' * 100)
+        sleep(2)
+
+        driver.back()
+
+        list = driver.find_element_by_class_name('prList')
+        a_tags = list.find_elements_by_tag_name('a')
+
+
+
+
+
