14ed0146d49e0e535d6ba0e684c664014395f19e
YoonSub <lefantopera@gmail.com>
2022-04-11 21:22:47 +0900

    - 웹크롤링 pd.read_html 이용 방법 후 pd.DataFrame 작성법

--- self.homework/220411 selenium_instagram.py
+++ self.homework/220411 selenium_instagram.py
@@ -12,16 +12,17 @@ path = 'C:/Users/crid2/driver/chromedriver.exe'
 driver = webdriver.Chrome(path)
 driver.get('https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=228')
 
-all_news = driver.find_element_by_css_selector('ul.type06_headline')
+all_news01 = driver.find_element_by_css_selector('ul.type06_headline')
+all_news02 = driver.find_element_by_css_selector('ul.type06')
+all_link = all_news01.find_elements_by_tag_name('a')
+all_link02 = all_news02.find_elements_by_tag_name('a')
 
-link = all_news.find_elements_by_tag_name('a')
 
-print(all_news.text)
-
-
-for i in range(1,20) :
-
-    print( i ,'번' ,link[i].text)
-    print(link[i].get_attribute('href'))
+for i in range(1,len(all_link),2) :
+    link_text = all_link[i].get_attribute('href')
+    print()
+    print( i-1 ,'번' ,all_link[i].text)
+    print(link_text)
     print('*' * 100)
+    print()
 
