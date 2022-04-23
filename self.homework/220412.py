c12a83499288036de3fc463840f29b03a64766bc
YoonSub <lefantopera@gmail.com>
2022-04-23 23:17:17 +0900

    웹크롤링 기능 구현

--- self.homework/220412.py
+++ self.homework/220412.py
@@ -1,20 +1,60 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
+import urllib.request
 path = 'C:/Users/crid2/driver/chromedriver.exe'
-driver = webdriver.Chrome(path)
 
-url =''
-driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+# url =''
+# driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+
+# mail_address = 'crid2450@gmail.com'
+# password = 'g5y013650e!'
+
+
+driver = webdriver.Chrome(path)
+driver.get('https://www.google.co.kr/imghp?hl=ko&ogbl')
+
 
 elem = driver.find_element_by_name('q')
-elem.send_keys('nude women')
+elem.send_keys('dream')
 elem.send_keys(Keys.RETURN)
-driver.find_elements_by_css_selector(' .bRMDJf.islir')[0].click()
-time.sleep(3)
-print(driver.find_element_by_css_selector('.n3VNCb').get_attribute('src'))
+images = driver.find_elements_by_css_selector(' .bRMDJf.islir')
 
-# elem.clear()
-# driver.close()
+# scroll down function
+SCROLL_PAUSE_TIME = 1
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try :
+            driver.find_element_by_css_selector('.mye4qd').click()
+        except :
+            break
+
+    last_height = new_height
+
+# image search and local download
+count = 1
+for image in images :
+    try :
+        image.click()
+        img_url = driver.find_element_by_css_selector('.n3VNCb').get_attribute('src')
+        urllib.request.urlretrieve(img_url, str(count) + '.jpg')
+        time.sleep(1)
+        count += 1
+    except :
+        pass
+
+elem.clear()
+driver.close()
 
